f6d78b2b5fbb0bb3117022a1b243a71257fdbac6
João Vitor Malvestio da Silva <joaovitormalvestio@gmail.com>
2019-10-04 21:29:42 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/anesthesia/AnesthesiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.anesthesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnesthesiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnesthesiaApplication.class, args);
+    }
+
+}
